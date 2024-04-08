7264c183b69b8ad04aa8b58c4c7b258ea000c1a8
sewalters <sew5720@psu.edu>
2024-04-08 12:50:24 -0700

    Fixed secondary axis bug with content widgets. (SEW)

--- src/embedded_flexbox.adb
+++ src/embedded_flexbox.adb
@@ -18,27 +18,29 @@ with Bitmap_Color_Conversion; use Bitmap_Color_Conversion;
 with event_handler;
 
 procedure Embedded_Flexbox is
+
    header  : Widget.Any_Acc :=
      Widget.Create
        (id         => "header1", parent => dui.main_widget,
         self_flex  =>
           (expand_w => (pixel, 250), expand_h => (pixel, 200),
            others   => <>),
-        child_flex => (dir => top_bottom, others => <>),
+        child_flex => (dir => left_right, others => <>),
         bgd        => HAL.Bitmap.Red);
    headerb  : Widget.Any_Acc :=
      Widget.Create
        (id         => "headerb", parent => header,
         self_flex  =>
-          (expand_w => (pixel, 250), expand_h => (pixel, 200),
+          (expand_w => (behavior => content), expand_h => (behavior => content),
            others   => <>),
-        child_flex => (dir => left_right, buoy => space_between, align => bottom, others => <>),
+        child_flex => (dir => left_right, others => <>),
         bgd        => HAL.Bitmap.Cyan);
+
    header2 : Widget.Any_Acc :=
      Widget.Button.Create
-       (id         => "header2", parent => headerb,
+       (id         => "header2", parent => headerb, 
         self_flex  =>
-          (expand_w => (pixel, 40), expand_h => (pixel, 40), align => center,
+          (expand_w => (pixel, 40), expand_h => (pixel, 40),
            others   => <>),
         child_flex => (dir => left_right, others => <>),
         --priority => 10,
@@ -47,7 +49,7 @@ procedure Embedded_Flexbox is
      Widget.Button.Create
        (id         => "header3", parent => headerb,
         self_flex  =>
-          (expand_w => (pixel, 100), expand_h => (pixel, 60),
+          (expand_w => (pixel, 40), expand_h => (pixel, 60),
            others   => <>),
         child_flex => (dir => left_right,  others => <>),
         --priority => 5,
@@ -64,6 +66,7 @@ procedure Embedded_Flexbox is
   --   head_t2 : Widget.Any_Acc := Widget.Text.Create(id => "text2", parent => header2, text => "2nd inserted, prio 10.");
   --   head_t3 : Widget.Any_Acc := Widget.Text.Create(id => "text3", parent => header3, text => "3rd inserted, prio 5.");
   --   head_t4 : Widget.Any_Acc := Widget.Text.Create(id => "text4", parent => header4, text => "4th inserted, prio 0.");
+
 begin
 
    dui.render (STM32.Board.Display.Width, STM32.Board.Display.Height);
