24192e3e182837939541dca30785131d48169591
Dom Carlucci <dfc5456@psu.edu>
2024-04-19 12:31:30 -0700

    fixed align bugs

--- src/kd_static.adb
+++ src/kd_static.adb
@@ -5,6 +5,7 @@ with Widget.Image;     use Widget.Image;
 with Widget.Button;    use Widget.Button;
 with Widget.Button.Switch; use Widget.Button.Switch;
 with Widget.Button.Buoy; use Widget.Button.Buoy;
+with Widget.Button.Dir; use Widget.Button.Dir;
 with HAL;              use HAL;
 with HAL.Bitmap;       use HAL.Bitmap;
 with Ada.Finalization; use Ada.Finalization;
@@ -21,23 +22,32 @@ bg1 : Widget.Any_Acc :=
       (expand_w => (behavior => max), expand_h => (behavior => max),
        others   => <>),
      child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Blue);
-     
+      (dir => top_bottom, others => <>),
+     bgd        => HAL.Bitmap.Pink);
 
-     ch1_bg1 : Widget.Any_Acc :=
+      button_hold : Widget.Any_Acc :=
    Widget.Create
-    (id         => "ch1_bg1", parent => bg1,
+    (id         => "button holder", parent => bg1,
+     self_flex  =>
+      (expand_w => (behavior => max), expand_h => (pixel, 50),
+       others   => <>),
+     child_flex =>
+      (dir => left_right, others => <>),
+     bgd        => HAL.Bitmap.Cyan);
+
+      widg_hold : Widget.Any_Acc :=
+   Widget.Create
+    (id         => "button holder", parent => bg1,
      self_flex  =>
       (expand_w => (behavior => max), expand_h => (behavior => max),
        others   => <>),
      child_flex =>
-      (dir => left_right, gap_c => (pixel, 15), align => bottom, others => <>),
+      (dir => left_right, buoy => space_around, align => center, others => <>),
      bgd        => HAL.Bitmap.Grey);
 
      child_of_red1 : Widget.Any_Acc :=
    Widget.Create
-    (id         => "child_of_red1", parent => ch1_bg1,
+    (id         => "child_of_red1", parent => widg_hold,
      self_flex  =>
       (expand_w => (pixel, 30), expand_h => (pixel, 30),
        others   => <>),
@@ -47,7 +57,7 @@ bg1 : Widget.Any_Acc :=
 
      child_of_red2 : Widget.Any_Acc :=
    Widget.Create
-    (id         => "child_of_red2", parent => ch1_bg1,
+    (id         => "child_of_red2", parent => widg_hold,
      self_flex  =>
       (expand_w => (pixel, 30), expand_h => (pixel, 30),
        others   => <>),
@@ -57,7 +67,7 @@ bg1 : Widget.Any_Acc :=
 
      child_of_red3 : Widget.Any_Acc :=
    Widget.Create
-    (id         => "child_of_red3", parent => ch1_bg1,
+    (id         => "child_of_red3", parent => widg_hold,
      self_flex  =>
       (expand_w => (pixel, 30), expand_h => (pixel, 30),
        others   => <>),
@@ -67,207 +77,34 @@ bg1 : Widget.Any_Acc :=
 
      child_of_red4 : Widget.Any_Acc :=
    Widget.Create
-    (id         => "child_of_red4", parent => ch1_bg1,
+    (id         => "child_of_red4", parent => widg_hold,
      self_flex  =>
       (expand_w => (pixel, 30), expand_h => (pixel, 30),
        others   => <>),
      child_flex =>
       (dir => left_right, others => <>),
      bgd        => HAL.Bitmap.Red);
-
-     ch2_bg1 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "ch2_bg1", parent => bg1,
-     self_flex  =>
-      (expand_w => (behavior => max), expand_h => (behavior => max),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, align => center, buoy => space_around, others => <>),
-     bgd        => HAL.Bitmap.Cyan);
-
-     child_of_cyan1 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_cyan1", parent => ch2_bg1,
-     self_flex  =>
-      (expand_w => (pixel, 20), expand_h => (pixel, 20),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Brown);
-
-     child_of_cyan2 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_cyan2", parent => ch2_bg1,
-     self_flex  =>
-      (expand_w => (pixel, 20), expand_h => (pixel, 20), align => bottom,
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.White);
-
-     child_of_cyan3 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_cyan3", parent => ch2_bg1,
-     self_flex  =>
-      (expand_w => (pixel, 20), expand_h => (pixel, 20),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Blue);
-
-     child_of_cyan4 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_cyan4", parent => ch2_bg1,
-     self_flex  =>
-      (expand_w => (pixel, 20), expand_h => (pixel, 20),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Red);
-
-     bg2 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "bg2", parent => dui.main_widget,
-     self_flex  =>
-      (expand_w => (behavior => max), expand_h => (behavior => max),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Blue);
-
-     ch1_bg2 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "ch1_bg2", parent => bg2,
-     self_flex  =>
-      (expand_w => (behavior => max), expand_h => (behavior => max),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, gap_c => (pixel, 15), others => <>),
-     bgd        => HAL.Bitmap.Cyan);
-
-     child_of_green1 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_green1", parent => ch1_bg2, priority => 3,
-     self_flex  =>
-      (expand_w => (pixel, 30), expand_h => (pixel, 30),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Brown);
-
-     child_of_green2 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_green2", parent => ch1_bg2, priority => 1,
-     self_flex  =>
-      (expand_w => (pixel, 30), expand_h => (pixel, 30),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.White);
-
-     child_of_green3 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_green3", parent => ch1_bg2, priority => 0,
-     self_flex  =>
-      (expand_w => (pixel, 30), expand_h => (pixel, 30),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Blue);
-
-     child_of_green4 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_green4", parent => ch1_bg2, priority => 5,
-     self_flex  =>
-      (expand_w => (pixel, 30), expand_h => (pixel, 30),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Red);
-
-     ch2_bg2 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "ch2_bg2", parent => bg2,
-     self_flex  =>
-      (expand_w => (behavior => max), expand_h => (behavior => max),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, align => bottom, others => <>),
-     bgd        => HAL.Bitmap.Grey);
-
-     content_wid : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "content_wid", parent => ch2_bg2,
-     self_flex  =>
-      (expand_w => (behavior => content), expand_h => (behavior => content),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Yellow);
-
-     child_of_pink1 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_pink1", parent => content_wid,
-     self_flex  =>
-      (expand_w => (pixel, 55), expand_h => (pixel, 30),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Brown);
-
-     child_of_pink2 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_pink2", parent => content_wid,
-     self_flex  =>
-      (expand_w => (pixel, 30), expand_h => (pixel, 60),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.White);
-
-     child_of_pink3 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_pink3", parent => content_wid,
-     self_flex  =>
-      (expand_w => (pixel, 30), expand_h => (pixel, 30),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Blue);
-
-     child_of_pink4 : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "child_of_pink4", parent => content_wid,
-     self_flex  =>
-      (expand_w => (pixel, 30), expand_h => (pixel, 30),
-       others   => <>),
-     child_flex =>
-      (dir => left_right, others => <>),
-     bgd        => HAL.Bitmap.Red);
-
-     button_hold : Widget.Any_Acc :=
-   Widget.Create
-    (id         => "button holder", parent => ch2_bg1,
-     self_flex  =>
-      (expand_w => (pixel, 50), expand_h => (behavior => max), align => right,
-       others   => <>),
-     child_flex =>
-      (dir => top_bottom, others => <>),
-     bgd        => HAL.Bitmap.Cyan);
 
      align_button : Widget.Any_Acc :=
    Widget.Button.Switch.Create
-    (id        => "a", parent => button_hold, text => "Alignment", target => ch2_bg1,
+    (id        => "a", parent => button_hold, text => "Alignment", target => widg_hold,
      self_flex =>
-      (expand_w => (pixel, 50), expand_h => (behavior => max), others => <>),
-     bgd       => HAL.Bitmap.Grey);
+      (expand_w => (behavior => max), expand_h => (behavior => max), others => <>),
+     bgd       => HAL.Bitmap.Cyan);
 
      buoy_button : Widget.Any_Acc :=
    Widget.Button.Buoy.Create
-    (id        => "b", parent => button_hold, text => "Buoyancy", target => ch2_bg1,
+    (id        => "b", parent => button_hold, text => "Buoyancy", target => widg_hold,
      self_flex =>
-      (expand_w => (pixel, 50), expand_h => (behavior => max), others => <>),
-     bgd       => HAL.Bitmap.Grey);
+      (expand_w => (behavior => max), expand_h => (behavior => max), others => <>),
+     bgd       => HAL.Bitmap.Cyan);
+
+     dir_button : Widget.Any_Acc :=
+   Widget.Button.Dir.Create
+    (id        => "b", parent => button_hold, text => "Direction", target => widg_hold,
+     self_flex =>
+      (expand_w => (behavior => max), expand_h => (behavior => max), others => <>),
+     bgd       => HAL.Bitmap.Cyan);
 
 begin
 
